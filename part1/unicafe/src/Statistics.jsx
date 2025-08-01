3c1d75797febf77f9223df63c6a017fab04d9258
Swikar Ramdam <swikarramdam@gmail.com>
2025-08-01 18:35:11 +0545

    1.12 completed

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -9,12 +9,42 @@ const Statistics = ({good, neutral, bad}) => {
 
   return (
     <div>
-       <StatisticLine text = "good" value = {good}/>
+      <table>
+        <tbody>
+      <tr>
+        <td>good  </td>
+        <td>{good}</td>
+      </tr>
+      <tr>
+        <td>neutral  </td>
+        <td>{neutral}</td>
+      </tr>
+      <tr>
+        <td>bad  </td>
+        <td>{bad}</td>
+      </tr>
+      <tr>
+        <td>all  </td>
+        <td>{all}</td>
+      </tr>
+      <tr>
+        <td>average  </td>
+        <td>{average}</td>
+      </tr>
+      <tr>
+        <td>positive  </td>
+        <td>{positive}</td>
+      </tr>
+        </tbody>
+      </table>
+
+    {/* Modified for 1.11 */}
+       {/* <StatisticLine text = "good" value = {good}/>
        <StatisticLine text = "bad" value = {bad}/>
        <StatisticLine text = "neutral" value = {neutral}/>
        <StatisticLine text = "all" value = {all}/>
        <StatisticLine text = "average" value = {average.toFixed(2) + "%"}/>
-       <StatisticLine text = "positive" value = {positive.toFixed(2)}/>
+       <StatisticLine text = "positive" value = {positive.toFixed(2)}/> */}
     </div>
   )
 }
