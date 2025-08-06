b3cd1dc1db9bed0db1199dc245dcc471913b4d06
Swikar Ramdam <swikarramdam@gmail.com>
2025-08-06 18:21:00 +0545

    done with part2c

---
+++ part2(c)/phonebook/src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import Filter from "./Components/Filter";
+import Form from "./Components/Form";
+import Display from "./Components/Display";
+import axios from "axios";
+
+const App = () => {
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [persons, setPersons] = useState([]);
+  useEffect(function () {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+    console.log(persons);
+  }, []);
+
+  const filtered = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        // persons={persons}
+      />
+      <h2>Add a new</h2>
+      <Form
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        newNumber={newNumber}
+        setNewName={setNewName}
+        setNewNumber={setNewNumber}
+      />
+      <h2>Numbers</h2>
+
+      <Display filtered={filtered} />
+    </div>
+  );
+};
+
+export default App;
+
+// const persons = fetched.data;
+
+// const [persons, setPersons] = useState([
+//   { name: "Arto Hellas", number: "040-123456", id: 1 },
+//   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+//   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+//   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+// ]);
