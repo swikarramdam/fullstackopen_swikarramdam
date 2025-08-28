36d17b3d016c97c698bc0f88bb570ede1b40b9d0
Swikar Ramdam <swikarramdam@gmail.com>
2025-08-28 19:57:18 +0545

    moving to part 5

---
+++ part 4/utils/logger.js
@@ -0,0 +1,10 @@
+// utils/logger.js
+const info = (...params) => {
+  console.log(...params);
+};
+
+const error = (...params) => {
+  console.error(...params);
+};
+
+module.exports = { info, error };
