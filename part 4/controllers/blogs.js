973b3ef26eb1fd6af292856e940c90752385d728
Swikar Ramdam <swikarramdam@gmail.com>
2025-08-30 17:42:07 +0545

    well going good upto 4.21

--- part 4/controllers/blogs.js
+++ part 4/controllers/blogs.js
@@ -1,11 +1,26 @@
 //controllers/blogs.js
 const blogsRouter = require("express").Router();
-const { createBlogs, getAllBlogs, updateBlog } = require("./blogsController");
+const {
+  createBlog,
+  getAllBlogs,
+  updateBlog,
+  deleteBlog,
+} = require("./blogsController");
 const { tokenExtractor, userExtractor } = require("../utils/middleware");
 
 blogsRouter.get("/", getAllBlogs);
-blogsRouter.post("/", createBlogs);
-blogsRouter.put("/", updateBlog);
-blogsRouter.post("/", tokenExtractor, userExtractor, createBlogs);
+blogsRouter.post("/", tokenExtractor, userExtractor, createBlog);
+blogsRouter.put("/:id", updateBlog);
+// blogsRouter.delete("/:id", tokenExtractor, userExtractor, deleteBlog);
+blogsRouter.delete(
+  "/:id",
+  tokenExtractor,
+  userExtractor,
+  (req, res, next) => {
+    console.log("DELETE route hit!", req.params.id);
+    next();
+  },
+  deleteBlog
+);
 
 module.exports = blogsRouter;
