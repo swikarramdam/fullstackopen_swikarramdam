c7acdf190a8c1af5e4f6de34245ed5d1e591f50b
Swikar Ramdam <swikarramdam@gmail.com>
2025-09-07 19:03:25 +0545

    5.19

--- part 5/bloglist-frontend/tests/blogApp.spec.jsx
+++ part 5/bloglist-frontend/tests/blogApp.spec.jsx
@@ -31,4 +31,31 @@ test.describe("Blog app", () => {
       await expect(page.getByRole("button", { name: /login/i })).toBeVisible();
     });
   });
+
+  test.describe("When logged in", () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto("http://localhost:5173");
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      //login
+      await page.getByLabel("username").fill("mynewuser");
+      await page.getByLabel("password").fill("mypassword123");
+      await page.getByRole("button", { name: /login/i }).click();
+
+      //expand
+      await page.getByRole("button", { name: /Create new blog/i }).click();
+
+      //after login
+      await page.getByLabel("title").fill("My input title");
+      await page.getByLabel("author").fill("My input author");
+      await page.getByLabel("url").fill("My input url");
+      await page.getByRole("button", { name: /create/i }).click();
+
+      //check
+      await expect(
+        page.getByText("My input title", { exact: true })
+      ).toBeVisible();
+    });
+  });
 });
