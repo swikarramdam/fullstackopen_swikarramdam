7653562c7af1396737dcdf96c7f85a257a72f62a
swikarramdam <swikarramdam@gmail.com>
2025-10-18 09:55:22 +0545

    7.5

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -103,9 +103,11 @@ const CreateNew = (props) => {
       votes: 0,
     });
     navigate("/");
-    setContent("");
-    setAuthor("");
-    setInfo("");
+  };
+  const resetFields = () => {
+    content.reset();
+    author.reset();
+    info.reset();
   };
 
   return (
@@ -129,7 +131,10 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="button" onClick={resetFields}>
+          reset
+        </button>
+        <button type="submit">create</button>
       </form>
     </div>
   );
