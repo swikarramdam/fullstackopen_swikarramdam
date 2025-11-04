3e3cc7deec4b0c3e1bdbbf091a18e539ad9c3d89
swikarramdam <swikarramdam@gmail.com>
2025-11-04 13:56:00 +0545

    added commands.sql

---
+++ part13/blog-app/commands.sql
@@ -0,0 +1,7 @@
+-- commands.sql
+
+INSERT INTO blogs (author, title, url, likes)
+VALUES ('Dan ramov', 'Why Does JavaScript Hate Me', 'https://overreacted.io', 0);
+
+INSERT INTO blogs (author, title, url, likes)
+VALUES ('Laurenz', 'Postgres Indexing Tips', 'https://use-the-index-luke.com', 5);
